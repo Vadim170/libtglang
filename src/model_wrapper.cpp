5614f149c0850bbb4a1ddaa8d2261551213f7ddf
Vadim Makarov <vadim.makarov@pushok.com>
2023-11-19 01:28:31 +0300

    added onnx 1.16.2

--- src/model_wrapper.cpp
+++ src/model_wrapper.cpp
@@ -1,10 +1,10 @@
 #include "model_wrapper.h"
-#include "model_data.h"
+#include "model_data_qs.h"
 
 unsigned char* get_resources_model_onnx() {
-	return resources_model_onnx;
+	return model_con2_8_80_2_quant_cp_with_runtime_opt_ort;
 }
 
 unsigned int get_resources_model_onnx_len() {
-	return resources_model_onnx_len;
+	return model_con2_8_80_2_quant_cp_with_runtime_opt_ort_len;
 }
